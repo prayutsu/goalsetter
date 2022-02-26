d0edb849f4f5c5b632b6f21a7ac65c95317d4988
Abhay Garg <gargabhay2000@gmail.com>
2022-02-26 10:56:17 +0530

    Save changes

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,9 @@ app.use("/api/users", require("./routes/userRoutes"));
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/build")));
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
   );
 } else {
   app.get("/", (req, res) => res.send("Please go in production."));
